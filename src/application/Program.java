c6993a816edbee0ce80bcbd9607338b67d3e1cba
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-05-05 17:35:38 -0300

    Program and Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args){
 
         Department obj=new Department(1,"Book");
         System.out.println(obj);
-
+        Seller sl=new Seller(21,"Bob Brown","bob@gmail.com",new Date(),3000.0,obj);
+        System.out.println(sl);
     }
 }

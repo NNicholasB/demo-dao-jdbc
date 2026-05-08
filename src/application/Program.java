0df3aa9846aee7aaa208a9bc91e820d622371c36
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-05-07 22:23:33 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n====TEST 3: seller findAll==========");
+        list =sld.findAll();
+        for (Seller obj:list){
+            System.out.println(obj);
+        }
     }
 }
